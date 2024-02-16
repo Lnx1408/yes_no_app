2cf24d0288c4b687ce508c7a93f81bac70e4b5a6
Lnx <yermin.linos@ug.edu.ec>
2024-02-15 23:29:56 -0500

    Appbar

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg'),
+          ),
+        ),
+        title: const Text("Mein Lieber"),
+        centerTitle: false,
+      ),
+    );
+  }
+}
