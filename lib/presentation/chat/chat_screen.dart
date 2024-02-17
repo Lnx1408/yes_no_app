cd844ab49fbc98391f23d9833db1a4004767577a
Lnx <yermin.linos@ug.edu.ec>
2024-02-16 20:46:03 -0500

    ListView

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -14,8 +14,31 @@ class ChatScreen extends StatelessWidget {
                 'https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg'),
           ),
         ),
-        title: const Text("Mein Lieber"),
+        title: const Text("User"),
         centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            Text('Hola'),
+            Text('Mundo')
+          ],
+        ),
       ),
     );
   }
