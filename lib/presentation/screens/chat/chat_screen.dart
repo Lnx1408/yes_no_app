7d7e47e816045ed3557954e0a2e41cae3ea5530d
Lnx <yermin.linos@ug.edu.ec>
2024-02-16 21:07:19 -0500

    WidgetMyMessage

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -32,11 +33,10 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(child: ListView.builder(
               itemBuilder: (context, index) {
-                return Text('Indice: $index');
+                return const MyMessageBubble();
               },
             )),
-            Text('Hola'),
-            Text('Mundo')
+            const Text('Mundo')
           ],
         ),
       ),
