5cd2d83d2aa4f63ea16e24d44ef91726516cc53a
Lnx <yermin.linos@ug.edu.ec>
2024-02-17 19:16:21 -0500

    Optimizar el mapper

--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,11 +1,32 @@
+import 'package:yes_no_app/domain/entities/message.dart';
+
 class YesNoModel {
-  String answer;
-  bool forced;
-  String image;
+  final String answer;
+  final bool forced;
+  final String image;
 
-  YesNoModel({required this.answer, required this.forced, required this.image});
+  YesNoModel({
+    required this.answer,
+    required this.forced,
+    required this.image,
+  });
 
-  //Factory constructor: me permite mapear la respuesta desde el constructor
   factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
-      answer: json['answer'], forced: json['forced'], image: json['image']);
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+      text: answer == 'yes' ? 'Sí' : 'No',
+      fromWho: FromWho.her,
+      imageUrl: image);
 }
+
+//Se puede agilizar el trabajo de mapeo usando https://quicktype.io/ como recomendación activar el null safety y las propiedades como finals
